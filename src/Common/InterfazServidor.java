73c6253e3f2a3232a793213c43c76066f646ee70
Oscar <oscar.rojas.g@mail.pucv.cl>
2024-05-03 11:59:30 -0400

    corregir método generarBoleta en InterfazServidor

--- src/Common/InterfazServidor.java
+++ src/Common/InterfazServidor.java
@@ -3,9 +3,10 @@ package Common;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface InterfazServidor extends Remote {
-    public void generarBoleta(Boleta boleta) throws RemoteException, SQLException;
+    public void generarBoleta(ArrayList<ItemCarrito> itemsCarrito, int idCajero) throws RemoteException, SQLException;
     public Item obtenerItem(int idProducto) throws RemoteException;
     public Boleta obtenerBoleta(int idBoleta) throws RemoteException, SQLException;
 }
